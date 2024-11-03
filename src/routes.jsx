3f221e80f23488d4e068281f8b883c8a028b8b9c
Zeref296 <thanhnc769@gmail.com>
2024-11-03 20:24:40 +0700

    Thành Thêm Landingpage, Login, SignUp

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+//  import Landingpage from "./pages/Landingpage";
+// import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        {/* <Route path="/board/:id" element={<BoardPage />} /> */}
+        {/* Route cho trang Landingpage */}
+        {/* <Route path="/" element={<Landingpage />} /> */}
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default AppRoutes;
