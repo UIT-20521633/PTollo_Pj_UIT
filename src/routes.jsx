dea799dd2e4b720d66fe50b970b905656e30c856
UIT-20521633 <20521633@gm.uit.edu.vn>
2024-11-15 21:54:16 +0700

    code ui

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-//  import Landingpage from "./pages/Landingpage";
-// import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Route cho trang Landingpage */}
-        {/* <Route path="/" element={<Landingpage />} /> */}
-        {/* <Route path="/" element={<Login />} /> */}
-        <Route path="/" element={<SignUp />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        {/* <Route path="/board/:id" element={<BoardPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default AppRoutes;
